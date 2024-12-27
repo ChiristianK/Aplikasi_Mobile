786cc0d9b5cecc8a2f0d3ccb6a98a12a65ce2c33
fithraa <fithra.ahmad90@gmail.com>
2024-12-27 08:52:40 +0700

    fix navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,25 @@
 import { Text, View } from "react-native";
 import { Link, NativeRouter, Route, Routes } from "react-router-native";
+import { AuthProvider } from './AuthContext';
 import AddData from "./AddData";
 import HomePage from "./HomePage";
 import EditData from "./EditData";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 export default function Index() {
   return (
-    <NativeRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/add" element={<AddData />} />
-        <Route path="/edit" element={<EditData />} />
-      </Routes>
-    </NativeRouter>
+    <AuthProvider>
+      <NativeRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/add" element={<AddData />} />
+          <Route path="/edit" element={<EditData />} />
+        </Routes>
+      </NativeRouter>
+    </AuthProvider>
 
   );
 }
